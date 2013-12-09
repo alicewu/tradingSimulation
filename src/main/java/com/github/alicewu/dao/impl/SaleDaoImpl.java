424fb3430d86fbefd7eec0a1158e156e25095c94
Alice Wu <alice_wu@mit.edu>
2013-12-09 01:21:58 -0500

    Create SaleDaoImpl.java

---
+++ src/main/java/com/github/alicewu/dao/impl/SaleDaoImpl.java
@@ -0,0 +1,34 @@
+package com.github.alicewu.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.github.alicewu.HibernateUtil;
+import com.github.alicewu.dao.SaleDao;
+import com.github.alicewu.domain.MarketOrder;
+import com.github.alicewu.domain.Quote;
+import com.github.alicewu.domain.Sale;
+
+public class SaleDaoImpl extends BaseDaoImpl<Sale> implements SaleDao {
+
+	public SaleDaoImpl() {
+		super(Sale.class);
+	}
+
+	public List<Sale> findByClient(int id) {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+
+		Query query = session.createQuery("FROM " + Sale.class.getName()
+				+ " WHERE buyerId = :id OR sellerId = :id");
+		query.setParameter("id", id);
+		List<Sale> sales = query.list();
+		session.flush();
+		session.close();
+		return sales;
+	}
+
+}
