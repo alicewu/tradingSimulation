66e9c0abf936e04f28a1b3188fe9fb80585b8530
Alice Wu <alice_wu@mit.edu>
2013-12-09 01:27:05 -0500

    Create Metadata.java

---
+++ src/main/java/com/github/alicewu/domain/Metadata.java
@@ -0,0 +1,102 @@
+package com.github.alicewu.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+@Entity
+public class Metadata implements BaseObject {
+
+	@Id
+	private int id;
+	private int last;
+	private int high;
+	private int low;
+	private int volume;
+	private int bid;
+	private int bidSize;
+	private int ask;
+	private int askSize;
+	@Version
+	private int version;
+
+	public Metadata() {
+	}
+
+	public int getHigh() {
+		return high;
+	}
+
+	public void setHigh(int high) {
+		this.high = high;
+	}
+
+	public int getLow() {
+		return low;
+	}
+
+	public void setLow(int low) {
+		this.low = low;
+	}
+
+	public int getLast() {
+		return last;
+	}
+
+	public void setLast(int last) {
+		this.last = last;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public int getBid() {
+		return bid;
+	}
+
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+
+	public int getBidSize() {
+		return bidSize;
+	}
+
+	public void setBidSize(int bidSize) {
+		this.bidSize = bidSize;
+	}
+
+	public int getAsk() {
+		return ask;
+	}
+
+	public void setAsk(int ask) {
+		this.ask = ask;
+	}
+
+	public int getAskSize() {
+		return askSize;
+	}
+
+	public void setAskSize(int askSize) {
+		this.askSize = askSize;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+	
+	public void addToVolume(int size) {
+		this.volume += size;
+	}
+
+}
