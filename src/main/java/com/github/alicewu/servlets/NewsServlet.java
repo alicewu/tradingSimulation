837bf62d422ddbf6c0056eb6e560660cbdff31f5
Alice Wu <spam02139@gmail.com>
2013-12-09 10:34:26 -0500

    Create NewsServlet.java

---
+++ src/main/java/com/github/alicewu/servlets/NewsServlet.java
@@ -0,0 +1,30 @@
+package com.github.alicewu.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.github.alicewu.services.NewsServices;
+
+public class NewsServlet extends HttpServlet {
+	
+	NewsServices newsServices = new NewsServices();
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setStatus(HttpServletResponse.SC_OK);
+		newsServices.getNews(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setStatus(HttpServletResponse.SC_OK);
+		newsServices.insertNews(request, response);
+	}
+
+}
