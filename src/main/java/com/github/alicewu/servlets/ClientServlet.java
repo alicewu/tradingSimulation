9508decc2712ff93775c14d7ffc4eb064e68022c
Alice Wu <spam02139@gmail.com>
2013-12-09 10:33:23 -0500

    Create ClientServlet.java

---
+++ src/main/java/com/github/alicewu/servlets/ClientServlet.java
@@ -0,0 +1,31 @@
+package com.github.alicewu.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.github.alicewu.services.ClientServices;
+
+public class ClientServlet extends HttpServlet {
+
+	ClientServices clientServices = new ClientServices();
+
+	// curl localhost:8080/client/?email=pandas
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.setContentType("text/html");
+		clientServices.getClient(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setStatus(HttpServletResponse.SC_OK);
+		clientServices.insertClient(request);
+	}
+
+}
