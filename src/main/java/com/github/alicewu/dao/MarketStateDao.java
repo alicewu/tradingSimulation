7a450900f1b435677d9d39b8abbc19cf63bd4e29
Alice Wu <alice_wu@mit.edu>
2013-12-09 01:02:00 -0500

    Create MarketStateDao.java

---
+++ src/main/java/com/github/alicewu/dao/MarketStateDao.java
@@ -0,0 +1,10 @@
+package com.github.alicewu.dao;
+
+import com.github.alicewu.domain.MarketState;
+
+public interface MarketStateDao extends BaseDao<MarketState> {
+	
+	MarketState getCurrentMarketState();
+	
+
+}
