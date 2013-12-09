d1881ae1c2a51232827eba5b4fab84460ea3680d
Alice Wu <alice_wu@mit.edu>
2013-12-09 01:00:26 -0500

    Create MarketOrderDao.java

---
+++ src/main/java/com/github/alicewu/dao/MarketOrderDao.java
@@ -0,0 +1,20 @@
+package com.github.alicewu.dao;
+
+import java.util.List;
+
+import com.github.syqnew.domain.MarketOrder;
+
+public interface MarketOrderDao extends BaseDao<MarketOrder> {
+
+	List<MarketOrder> getByClient(int client);
+
+	List<MarketOrder> getLimitBuys();
+
+	List<MarketOrder> getLimitSells();
+
+	List<MarketOrder> getMarketBuys();
+
+	List<MarketOrder> getMarketSells();
+	
+
+}
