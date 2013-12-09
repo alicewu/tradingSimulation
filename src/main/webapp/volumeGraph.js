2db8bac82c202c0c3d145ece7c1e7a14c76a5ff8
Alice Wu <spam02139@gmail.com>
2013-12-09 10:45:39 -0500

    Create volumeGraph.js

---
+++ src/main/webapp/volumeGraph.js
@@ -0,0 +1,52 @@
+var graphInterval, duration, timeLeft, min;
+
+function makeVolumeGraph(duration) {
+	min = new Date().getTime();
+
+	timeLeft = duration * 60 * 1000;
+	graphInterval = setInterval(renderVolumeGraph, 1000);
+}
+
+/* 
+ * Rendering volume graph from sale data. 
+ */
+function renderVolumeGraph() {
+	if (graphInterval > 0) {
+		timeLeft -= 1000;
+	} else {
+		clearInterval(graphInterval);
+	}
+
+	var dataSet = [];
+
+	var clientRequest = "clientId=" + "-1";
+	var ajax = $.ajax({
+		type : "GET",
+		url : "http://localhost:8080/sale",
+		data : clientRequest,
+		dataType : "json",
+		success : function(data) {
+			for ( var ct = 0; ct < data.length; ct++) {
+				dataSet.push([ data[ct]["time"], data[ct]["amount"] ]);
+			}
+			$.plot('#placeholder2', [ dataSet ], {
+				series : {
+					bars : {
+						show : true,
+						barWidth : 0.5,
+						align : "center"
+					}
+				},
+				xaxis : {
+					mode : "time",
+					timeformat : "%H:%M:%S", 
+					min : min
+				},
+				yaxis : {
+					min: 0
+				}
+			});
+
+		}
+	});
+}
