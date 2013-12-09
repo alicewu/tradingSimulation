6e3d4340ddf0ae23d1f38e0435fff9b27d738f10
Alice Wu <spam02139@gmail.com>
2013-12-09 10:18:29 -0500

    Create NewsServices.java

---
+++ src/main/java/com/github/alicewu/services/NewsServices.java
@@ -0,0 +1,45 @@
+package com.github.alicewu.services;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.github.alicewu.dao.NewsDao;
+import com.github.alicewu.dao.impl.NewsDaoImpl;
+import com.github.alicewu.domain.News;
+
+public class NewsServices {
+
+	NewsDao dao;
+
+	public NewsServices() {
+		dao = new NewsDaoImpl();
+	}
+
+	public void getNews(HttpServletRequest request, HttpServletResponse response)
+			throws JsonGenerationException, JsonMappingException, IOException {
+		String idStr = request.getParameter("id");
+		int id = Integer.parseInt(idStr);
+		News news = dao.getById(id);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		response.getWriter().println(mapper.writeValueAsString(news));
+	}
+
+	public void insertNews(HttpServletRequest request,
+			HttpServletResponse response) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		News[] news = mapper.readValue(request.getReader(), News[].class);
+		for (News newz : news) {
+			dao.persist(newz);
+		}
+	}
+}
