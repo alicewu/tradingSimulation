7d3c21b2e5205b635286a9849b6d92041f111bd8
Alice Wu <spam02139@gmail.com>
2013-12-09 10:32:38 -0500

    Create AdminServlet.java

---
+++ src/main/java/com/github/alicewu/servlets/AdminServlet.java
@@ -0,0 +1,34 @@
+package com.github.alicewu.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.github.alicewu.services.AdminServices;
+
+
+public class AdminServlet extends HttpServlet {
+	
+	private AdminServices adminServices = new AdminServices();
+	
+	// for students  
+	// In mySQL: set global transaction isolation level read committed
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setStatus(HttpServletResponse.SC_OK);
+		adminServices.getCurrentYear(request, response);
+	}
+
+	// For admins
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setStatus(HttpServletResponse.SC_OK);
+		adminServices.updateMarketState(request);
+	}
+
+}
