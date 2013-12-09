a2a339300ff646b067190242d85cfcf8e6488994
Alice Wu <spam02139@gmail.com>
2013-12-09 10:44:24 -0500

    Create trading.js

---
+++ src/main/webapp/trading.js
@@ -0,0 +1,89 @@
+function Trading(duration, clientId) {
+	this.duration = duration;
+	this.clientId = clientId;
+	this.timeLeft = duration * 60 * 1000;
+	this.timerId = null;
+}
+
+Trading.prototype.constructor = Trading;
+
+Trading.prototype.startTrading = function() {
+	this.timerId = setInterval(this.checkForUpdates, 1000);
+};
+
+/*
+ * Get market orders for a specific client and populate their open orders container in the ui
+ */
+Trading.prototype.checkForUpdates = function() {
+	if (this.timeLeft > 0)
+		this.timeLeft -= 1000;
+	else
+		clearInterval(timerId);
+
+	var shares;
+	var cash;
+	var price;
+
+	var requestStr = "clientId=" + this.clientId;
+	var ajax = $.ajax({
+		type : "GET",
+		url : "http://localhost:8080/metadata",
+		data : requestStr,
+		dataType : "json",
+		success : function(data) {
+			price = data[0]["last"];
+			shares = data[1]["shares"];
+			cash = data[1]["cash"];
+
+			var ajax2 = $.ajax({
+				type : "GET",
+				url : "http://localhost:8080/order",
+				data : requestStr,
+				dataType : "json",
+				success : function(data) {
+					if (data) {
+
+						var ordersList = [];
+						for ( var ct = 0; ct < data.length; ct++) {
+							var temp = {};
+							temp["id"] = data[ct]["id"];
+							if (data[ct]["unfulfilled"] == 0)
+								continue;
+							if (data[ct]["orderType"] == 1 && (price * data[ct]["unfulfilled"] <= cash)) {
+
+								temp["order"] = "Buy " + data[ct]["unfulfilled"] + " shares at Market Price";
+								temp["value"] = data[ct]["id"];
+							} else if (data[ct]["orderType"] == 2 && (data[ct]["unfulfilled"] <= shares)) {
+								temp["order"] = "Sell " + data[ct]["unfulfilled"] + " shares at Market Price";
+								temp["value"] = data[ct]["id"];
+							} else if (data[ct]["orderType"] == 3
+									&& (data[ct]["price"] * data[ct]["unfulfilled"] <= cash)) {
+								temp["order"] = "Buy " + data[ct]["unfulfilled"] + " shares at $" + data[ct]["price"];
+								temp["value"] = data[ct]["id"];
+							} else if (data[ct]["orderType"] == 4 && (data[ct]["unfulfilled"] <= shares)) {
+
+								temp["order"] = "Sell " + data[ct]["unfulfilled"] + " shares at $" + data[ct]["price"];
+								temp["value"] = data[ct]["id"];
+							} else {
+								//
+								console.log(data[ct]);
+								// var ajax = $.ajax({
+								// type : "POST",
+								// url :
+								// "http://localhost:8080/sale",
+								// dataType : "json"
+								// data : data[ct],
+								// });
+							}
+
+							ordersList.push(temp);
+						}
+						$('#openOrdersTable').html(_openOrdersTemplate(ordersList));
+						$('#cancelOrder').html(_cancelOrderTemplate(ordersList));
+					}
+				}
+			});
+		}
+	});
+
+};
