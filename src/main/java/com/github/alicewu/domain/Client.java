ab9d02ac4571b324d47c58859d32a52749e3c386
Alice Wu <alice_wu@mit.edu>
2013-12-09 01:24:54 -0500

    Create Client.java

---
+++ src/main/java/com/github/alicewu/domain/Client.java
@@ -0,0 +1,67 @@
+package com.github.alicewu.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Client implements BaseObject {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String email;
+	private int cash = 10000;
+	private int shares = 400;
+
+	public Client() {
+	}
+
+	// Each client starts out with $10,000 and 400 shares
+	public Client(String email) {
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getCash() {
+		return cash;
+	}
+
+	public void setCash(int cash) {
+		this.cash = cash;
+	}
+
+	public int getShares() {
+		return shares;
+	}
+
+	public void setShares(int shares) {
+		this.shares = shares;
+	}
+
+	public void buyShares(int shares, int totalPrice) {
+		this.shares += shares;
+		this.cash -= totalPrice;
+	}
+
+	public void sellShares(int shares, int totalPrice) {
+		this.shares -= shares;
+		this.cash += totalPrice;
+	}
+
+}
