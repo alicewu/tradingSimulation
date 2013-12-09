c0a11d6dee7999d2646f2d441b8546097f3c6fbd
Alice Wu <alice_wu@mit.edu>
2013-12-09 01:05:59 -0500

    Create SaleDao.java

---
+++ src/main/java/com/github/alicewu/dao/SaleDao.java
@@ -0,0 +1,10 @@
+package com.github.alicewu.dao;
+
+import java.util.List;
+
+import com.github.alicewu.domain.Sale;
+
+public interface SaleDao extends BaseDao<Sale> {
+
+	List<Sale> findByClient(int id);
+}
