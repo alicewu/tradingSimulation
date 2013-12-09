24dccade4125a827c82b6ef85b0ccbe1bd7ac8a2
Alice Wu <alice_wu@mit.edu>
2013-12-09 00:57:18 -0500

    Create ClientDao.java

---
+++ src/main/java/com/github/alicewu/dao/ClientDao.java
@@ -0,0 +1,11 @@
+package com.github.alicewu.dao;
+
+import com.github.alicewu.domain.Client;
+
+public interface ClientDao extends BaseDao<Client> {
+	
+	Client findByEmail(String email);
+	
+	Client findById(int id);
+
+}
