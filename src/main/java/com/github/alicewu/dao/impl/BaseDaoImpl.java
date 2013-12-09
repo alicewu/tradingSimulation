97781fced3e637aba131fa6bea02739a4a3af3a1
Alice Wu <alice_wu@mit.edu>
2013-12-09 01:07:05 -0500

    Create BaseDaoImpl.java

---
+++ src/main/java/com/github/alicewu/dao/impl/BaseDaoImpl.java
@@ -0,0 +1,62 @@
+package com.github.alicewu.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.github.alicewu.HibernateUtil;
+import com.github.alicewu.dao.BaseDao;
+
+public class BaseDaoImpl<T> implements BaseDao<T> {
+	private Class<T> type;
+
+	public BaseDaoImpl(Class<T> type) {
+		this.type = type;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> findAll() {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+
+		List<T> objects = session.createQuery("from " + type.getName()).list();
+		session.close();
+		return objects;
+	}
+
+	public void persist(T BaseObject) {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.save(type.getName(), BaseObject);
+		session.getTransaction().commit();
+		session.flush();
+		session.close();
+	}
+
+	public void merge(T BaseObject) {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.merge(type.getName(), BaseObject);
+		session.getTransaction().commit();
+		session.flush();
+		session.close();
+	}
+
+	public void remove(T BaseObject) {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.delete(type.getName(), BaseObject);
+		session.getTransaction().commit();
+		session.flush();
+		session.close();
+	}
+
+	public int count() {
+		return findAll().size();
+	}
+
+}
