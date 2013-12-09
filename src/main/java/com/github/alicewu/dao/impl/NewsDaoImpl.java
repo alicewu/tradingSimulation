b907a7959fbc73e99c332c06a789027bcf574047
Alice Wu <alice_wu@mit.edu>
2013-12-09 01:20:01 -0500

    Create NewsDaoImpl.java

---
+++ src/main/java/com/github/alicewu/dao/impl/NewsDaoImpl.java
@@ -0,0 +1,25 @@
+package com.github.alicewu.dao.impl;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.github.alicewu.HibernateUtil;
+import com.github.alicewu.dao.NewsDao;
+import com.github.alicewu.domain.News;
+
+public class NewsDaoImpl extends BaseDaoImpl<News> implements NewsDao {
+	
+	public NewsDaoImpl() {
+		super(News.class);
+	}
+
+	public News getById(int id) {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+        
+        News news = (News) session.get(News.class, id);
+        session.close();
+        return news;
+	}
+
+}
